e3d7dc0a00207772f1bd9ddb03de9fd13f30d914
XaiverYuan <40052821+XaiverYuan@users.noreply.github.com>
2019-05-14 22:23:00 -0400

    easier output
    I think the time we called the particular number is somehow connected to the Fibonacci series. And I write code to output it. This would make it easier to compare the relationship between the number of calling and the Fibonacci series

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-static int arr[50];//this is used for storing value for space
-static int time[50];//how many times they asked for value in i the place
+#define MAX (50)
+static int arr[MAX];//this is used for storing value for space
+static int time[MAX];//how many times they asked for value in i the place
 int normal(int n){
   time[n]++;//to see how many times they ask value for n th number
   return n<3?1:normal(n-1)+normal(n-2);
@@ -17,6 +18,24 @@ int spaceX(int n){//this is the interface, some how like java public method
   arr[1]=arr[2]=1;
   return space(n);
 }
+void write_time(FILE *fp){
+  fprintf(fp,"index  called time\n");
+  for(int i=0;i<MAX;i++){
+    fprintf(fp,"%5d  %10d\n",i,time[i]);
+  }
+}
+void write_arr(FILE *fp){
+  fprintf(fp,"index  Fibonacci\n");
+  for(int i=0;i<MAX;i++){
+    fprintf(fp,"%5d  %10d\n",i,arr[i]);
+  }
+}
+void show_time(){
+  write_time(stdout);
+}
+void show_arr(){
+  write_arr(stdout);
+}
 int iterative(int n){//no recursive
   int a=1;
   int b=1;
@@ -30,7 +49,7 @@ int iterative(int n){//no recursive
 }
 int main(){
   printf("%d\n",normal(44));
-  for (int i=0;i<50;i++){
-    printf("%2d %5d\n",i,time[i]);
-  }
+  printf("%d\n",spaceX(44));
+  show_arr();
+  show_time();
 }
